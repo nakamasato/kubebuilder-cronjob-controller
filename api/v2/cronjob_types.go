a33dafe6e0fcb429429fc21bca025da146ed30c5
Masato Naka <masatonaka1989@gmail.com>
2022-03-09 05:58:00 +0900

    2.1. Add new version v2 for CronJob

--- api/v2/cronjob_types.go
+++ api/v2/cronjob_types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
+package v2
 
 import (
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
@@ -22,34 +22,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// ConcurrencyPolicy describes how the job will be handled.
-// Only one of the following concurrent policies may be specified.
-// If none of the following policies is specified, the default one
-// is AllowConcurrent.
-// +kubebuilder:validation:Enum=Allow;Forbid;Replace
-type ConcurrencyPolicy string
-
-const (
-	// AllowConcurrent allows CronJobs to run concurrently.
-	AllowConcurrent ConcurrencyPolicy = "Allow"
-
-	// ForbidConcurrent forbids concurrent runs, skipping next run if previous
-	// hasn't finished yet.
-	ForbidConcurrent ConcurrencyPolicy = "Forbid"
-
-	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
-	ReplaceConcurrent ConcurrencyPolicy = "Replace"
-)
-
 // CronJobSpec defines the desired state of CronJob
 type CronJobSpec struct {
-	//+kubebuilder:validation:MinLength=0
 
 	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
-	Schedule string `json:"schedule"`
+	Schedule CronSchedule `json:"schedule"`
 
 	//+kubebuilder:validation:Minimum=0
 
@@ -87,8 +64,52 @@ type CronJobSpec struct {
 	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
 
+// describes a Cron schedule.
+type CronSchedule struct {
+	// specifies the minute during which the job executes.
+	// +optional
+	Minute *CronField `json:"minute,omitempty"`
+	// specifies the hour during which the job executes.
+	// +optional
+	Hour *CronField `json:"hour,omitempty"`
+	// specifies the day of the month during which the job executes.
+	// +optional
+	DayOfMonth *CronField `json:"dayOfMonth,omitempty"`
+	// specifies the month during which the job executes.
+	// +optional
+	Month *CronField `json:"month,omitempty"`
+	// specifies the day of the week during which the job executes.
+	// +optional
+	DayOfWeek *CronField `json:"dayOfWeek,omitempty"`
+}
+
+// represents a Cron field specifier.
+type CronField string
+
+// ConcurrencyPolicy describes how the job will be handled.
+// Only one of the following concurrent policies may be specified.
+// If none of the following policies is specified, the default one
+// is AllowConcurrent.
+// +kubebuilder:validation:Enum=Allow;Forbid;Replace
+type ConcurrencyPolicy string
+
+const (
+	// AllowConcurrent allows CronJobs to run concurrently.
+	AllowConcurrent ConcurrencyPolicy = "Allow"
+
+	// ForbidConcurrent forbids concurrent runs, skipping next run if previous
+	// hasn't finished yet.
+	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+
+	// ReplaceConcurrent cancels currently running job and replaces it with a new one.
+	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+)
+
 // CronJobStatus defines the observed state of CronJob
 type CronJobStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
 	// A list of pointers to currently running jobs.
 	// +optional
 	Active []corev1.ObjectReference `json:"active,omitempty"`
@@ -100,7 +121,6 @@ type CronJobStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-//+kubebuilder:storageversion
 
 // CronJob is the Schema for the cronjobs API
 type CronJob struct {
--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	batchv1 "tutorial.kubebuilder.io/api/v1"
+	batchv2 "tutorial.kubebuilder.io/api/v2"
 	"tutorial.kubebuilder.io/controllers"
 	//+kubebuilder:scaffold:imports
 )
@@ -45,6 +46,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(batchv1.AddToScheme(scheme))
+	utilruntime.Must(batchv2.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
